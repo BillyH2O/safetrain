32607f75a5c5eb2e8ddafc89c1054b64bea4bb82
Billy <billy.billio.bones@gmail.com>
2025-01-03 04:54:26 +0100

    🎨  F06 : Apparence de la page initial (0 chat)

--- src/app/components/Messages.tsx
+++ src/app/components/Messages.tsx
@@ -14,9 +14,11 @@ export default function Messages({messages}: MessagesProps) {
             <Message key={i} content={message.content} isUserMessage={message.role === "user"}/>
         )) : (
         <div className='flex h-full flex-col items-center justify-center gap-2'>
-          <MessageSquare className="size-8 text-orange-500"/>
-            <h3 className='font-semibold text-xl text-white'>Chat opérationnel !</h3>
-            <p className="text-zinc-500 text-sm">Posez votre première question</p>
+          
+            <h3 className='text-center font-medium text-6xl text-white w-[65%]'>Simplifiez vos PDF grâce à notre  
+              <span className="text-gradient"> Agent Autonome</span>
+            </h3>
+            {/*<MessageSquare className="size-8 text-orange-500"/>*/}
         </div>
       )} 
     </div>
