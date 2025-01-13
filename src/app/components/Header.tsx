2db865fc26293f8acffcdcd291663f50e71a611a
Billy <billy.billio.bones@gmail.com>
2025-01-13 12:17:41 +0100

    ♻️ F09 : Logo refactorisation

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -3,9 +3,9 @@ import { ClerkProvider, SignInButton, SignedIn, SignedOut, UserButton } from '@c
 import { Bell } from 'lucide-react'
 import Image from 'next/image';
 import CYtechLogo from "../../assets/cy-logo.png";
-import SafetrainLogo from "../../assets/safetrain-logo.png";
 import TokenLogo from "../../assets/token-logo.png";
 import GemmeLogo from "../../assets/gemme-logo.png";
+import Logo from './Logo';
 
 type Props = {}
 
@@ -18,9 +18,9 @@ const Header = (props: Props) => {
         <SignedIn>
         <div className='flex items-center justify-between h-full'>
             <div className='flex justify-center items-center gap-10'>
-                <div className='flex justify-center items-center gap-5'>
-                    <Image src={SafetrainLogo} alt="logo open ai" height={35}/>
-                    <h2 className='antialiased text-xl font-mono'>Safetrain IA</h2>
+                <div className='flex justify-center items-center gap-2'>
+                    <Logo isClickable={true}/>
+                    <div className="font-medium text-black dark:text-white whitespace-pre antialiased text-xl font-mono">SafeTrain IA</div>
                 </div>
                 <Image src={CYtechLogo} alt="logo open ai" height={35}/>
             </div>
---
+++ src/app/components/Logo.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import React from "react";
+import Link from "next/link";
+import { motion } from "framer-motion";
+import SafetrainLogo from "../../assets/safetrain-logo.png"
+import Image from "next/image";
+
+type LogoProps = {
+  isClickable: boolean;
+  size?: "sm" | "ls" | "xl";
+};
+
+const sizeMap = {
+  sm: { width: 35},
+  ls: { width: 40},
+  xl: { width: 60},
+};
+
+const Logo = ({ isClickable, size = "sm" }: LogoProps) => {
+  const { width } = sizeMap[size];
+
+  const content = (
+    <div
+      className={`font-normal flex space-x-2 items-center text-sm text-black py-1 relative z-20 ${
+        isClickable ? "cursor-pointer" : ""
+      }`}
+    >
+      <div className="flex-shrink-0 mr-3" style={{ width }}>
+        <Image
+          src={SafetrainLogo}
+          alt="SafeTrain Logo"
+          className="rounded-lg"
+          layout="responsive"
+          width={width}
+        />
+      </div>
+    </div>
+  );
+
+  return isClickable ? (
+    <Link href="/" passHref>
+      {content}
+    </Link>
+  ) : (
+    content
+  );
+};
+
+export default Logo;
